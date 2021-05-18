ba09ac470657c2d3fbbdede037f574af5c528b9f
Antti Väisänen <antti.vaisanen@norsunluutorni.fi>
2021-05-18 14:02:11 +0000

    adding remote backend

--- variables.tf
+++ variables.tf
@@ -7,11 +7,12 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
+  default     = "anttiv"
 }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "North Europe"
 }
 
 variable "address_space" {
